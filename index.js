171680ea99c78cdee216a7b4967f199f13ba6882
lenovo <Athif binu>
2022-09-29 21:16:18 +0530

    finish

---
+++ index.js
@@ -0,0 +1,25 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);     //bto is used to creation of base 64 in encript
+
+  const linkInput = document.querySelector('#link-input');    //automatic select link
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
